4f8f4f3e2e514dad4062e58cc06ac7d1be8cafb0
阳林 <yl25488@ly.com>
2017-07-03 18:28:09 +0800

    【新增】数子转换中文
    功能要求：
    1。如果数字从0开始，读数应该是一位数的（例如“000”被读为“零零零”），“002”被读为“零零二”，“040”被读为“零四零”）。
    2。如果数字从1到9开始，读出应该是正常的英文基数。
    如“100”被称为“一百”，“120”被读为“一百二十”，“102”被读出为“一百零二”

---
+++ NumberToChinese/NumberToChinese/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NumberToChinese
+{
+    class Program
+    {
+        private static readonly DigitToChnText digitToChnText = new DigitToChnText();
+        static void Main(string[] args)
+        {
+            string result = string.Empty;
+            do
+            {
+                string num = string.Empty;
+                Console.WriteLine("请输入是3位的整数字符串：");
+                do
+                {
+                    num = Console.ReadLine();
+                    if (!IsNumeric(num))
+                    {
+                        Console.WriteLine("匹配失败！");
+                        Console.WriteLine("请输入是3位的整数字符串：");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                } while (true);
+                if (num.StartsWith("0"))
+                {
+                    num = NumberHelper.NumberToChinese2(num);
+                }
+                else
+                {
+                    num = NumberHelper.NumberToChinese(num);
+                    //不限制数字长度时使用
+                    //num = digitToChnText.Convert(num, false);
+                }
+                Console.WriteLine("输入为：" + num);
+                Console.Write("输入y继续或任意字符结束：");
+                result = Console.ReadLine();
+            } while (result.ToLower() == "y");
+        }
+        private static bool IsNumeric(string message)
+        {
+            if (message != "" && Regex.IsMatch(message, @"^\d{3}$"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
